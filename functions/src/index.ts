5770de675ebde4a54e6760a3e5ae0ed2497e8841
Aksels Ledins <aksels.ledins@gmail.com>
2020-05-16 16:23:54 +0800

    add export function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,34 @@
 import * as functions from 'firebase-functions';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const cors = require('cors');
+const corsHandler = cors();
+
+// The Firebase Admin SDK to access the Cloud Firestore.
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+export const exportData = functions.region('asia-east2').https.onRequest((request, response) =>
+  corsHandler(request, response, async () => {
+    if (!request || !request.headers || !request.headers.authorization) {
+      response.status(403).send('Unauthorized');
+      return;
+    }
+    // @ts-ignore
+    const idToken = request.headers.authorization.split('Bearer ')[1];
+    const user = await admin.auth().verifyIdToken(idToken);
+
+    if (!['ETWYO4lPKSSq5IclOSvha2etFz82', 'teBxhoPLshQh8DO38ssoWXNms6h2'].includes(user.uid)) {
+      response.status(403).send('Unauthorized');
+      return;
+    }
+
+
+    // retrieve all data // expensive request
+    const snapshot = await admin.firestore().collection('stores').get();
+    const docs = snapshot.docs.map((d: any) => ({ id: d.id, ...d.data() }));
+
+
+    response.status(200).send(docs);
+    return;
+  })
+);
